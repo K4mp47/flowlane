2e7c56bece716196897decd4be07634526b83dd3
Alberto Campagnolo <67239535+K4mp47@users.noreply.github.com>
2026-07-22 21:33:52 +0200

    Load final theme-aware polish layer

--- src/main.tsx
+++ src/main.tsx
@@ -14,6 +14,7 @@ import './palette.css'
 import './product-v2.css'
 import './design-system.css'
 import './phase6-compat.css'
+import './theme-ui-polish.css'
 import App from './App'
 
 const queryClient = new QueryClient({
