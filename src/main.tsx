6231dd68bb4a74de0409c85ce785b1444ab64d58
Alberto Campagnolo <67239535+K4mp47@users.noreply.github.com>
2026-07-22 21:10:26 +0200

    Remove legacy Astryx stylesheet imports

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import '@/components/ui/reset.css'
-import '@/components/ui/astryx.css'
 import './index.css'
 import './kanban-fixes.css'
 import './astryx-controls.css'
